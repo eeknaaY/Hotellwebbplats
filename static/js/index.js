ed6118369b4676b6245c7aaa18df6edaa6bf6a98
Johannes <johanneshageus@hotmail.com>
2023-03-07 12:54:36 +0100

    nvm now fixed js

--- static/js/index.js
+++ static/js/index.js
@@ -14,21 +14,22 @@ function test() {
 
 function changeBackground() {
     let color = '#5e5e5e';
-    if (window.location.href.endsWith("/") || window.location.href.endsWith("/index.html")) {
+    let endurl = window.location.pathname;
+    if (endurl.endsWith("/index.html") || endurl.endsWith("/")) {
         document.getElementById('startBtn').style.background = color;
         document.getElementById('startBtnComputer').style.background = color;
     }
 
-    else if (window.location.href.endsWith("/hitta.html")) {
+    else if (endurl.endsWith("/hitta.html")) {
         document.getElementById('hittaBtn').style.background = color;
         document.getElementById('hittaBtnComputer').style.background = color;
     }
-    else if (window.location.href.endsWith("/boka.html")) {
+    else if (endurl.endsWith("/boka.html")) {
         document.getElementById('bokaBtn').style.background = color;
         document.getElementById('bokaBtnComputer').style.background = color;
     }
 
-    else if (window.location.href.endsWith("kontakt.html")) {
+    else if (endurl.endsWith("kontakt.html")) {
         document.getElementById('kontaktBtn').style.background = color;
         document.getElementById('kontaktBtnComputer').style.background = color;
     }
