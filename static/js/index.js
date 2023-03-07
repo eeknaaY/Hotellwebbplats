3f1dd82b697c3a550488733eec75b3c687268b93
Johannes <johanneshageus@hotmail.com>
2023-03-07 12:43:59 +0100

    fix js, css changes

--- static/js/index.js
+++ static/js/index.js
@@ -14,21 +14,21 @@ function test() {
 
 function changeBackground() {
     let color = '#5e5e5e';
-    if (window.location.href == "http://127.0.0.1:5500/" || window.location.href == "http://127.0.0.1:5500/index.html") {
+    if (window.location.href.endsWith("/") || window.location.href.endsWith("/index.html")) {
         document.getElementById('startBtn').style.background = color;
         document.getElementById('startBtnComputer').style.background = color;
     }
 
-    else if (window.location.href == "http://127.0.0.1:5500/hitta.html") {
+    else if (window.location.href.endsWith("/hitta.html")) {
         document.getElementById('hittaBtn').style.background = color;
         document.getElementById('hittaBtnComputer').style.background = color;
     }
-    else if (window.location.href == "http://127.0.0.1:5500/boka.html") {
+    else if (window.location.href.endsWith("/boka.html")) {
         document.getElementById('bokaBtn').style.background = color;
         document.getElementById('bokaBtnComputer').style.background = color;
     }
 
-    else if (window.location.href == "http://127.0.0.1:5500/kontakt.html") {
+    else if (window.location.href.endsWith("kontakt.html")) {
         document.getElementById('kontaktBtn').style.background = color;
         document.getElementById('kontaktBtnComputer').style.background = color;
     }
